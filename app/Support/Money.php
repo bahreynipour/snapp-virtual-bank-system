c97a0079de5355add864429dc4ef186546efa24b
bahreynipour <bahreynipour@gmail.com>
2024-06-28 14:24:23 +0330

    feat: add Money value object
    in this case, we are sure that we are using immutable money objects and our app stay in a valid state

---
+++ app/Support/Money.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Support;
+
+use App\Exceptions\InvalidMoneyException;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+use Throwable;
+
+final readonly class Money
+{
+    /**
+     * @throws Throwable
+     */
+    private function __construct(private string|int|float $amount)
+    {
+        throw_if(
+            !is_numeric($this->amount),
+            InvalidArgumentException::class
+        );
+    }
+
+    /**
+     * build Money Object
+     * This object accepts persian and arabic digits by normalizing them.
+     *
+     * @throws Throwable
+     */
+    public static function forge(string|int|float $amount): Money
+    {
+        return new self(Str::normalizeNumbers($amount));
+    }
+
+    /**
+     * @throws Throwable
+     */
+    private function ensureAmount(string|int|float|Money $amount): float|int|string
+    {
+        return $amount instanceof Money
+            ? $amount->getAmount()
+            : Money::forge($amount)->getAmount();
+    }
+
+    public function __toString(): string
+    {
+        return $this->getAmount();
+    }
+
+    public function getAmount(): int|float|string
+    {
+        return $this->amount;
+    }
+
+    /**
+     * This money amount plus input amount
+     *
+     * @throws Throwable
+     */
+    public function plus(string|int|float|Money $amount): Money
+    {
+        return new self($this->getAmount() + $this->ensureAmount($amount));
+    }
+
+    /**
+     * This money amount minus input amount
+     *
+     * @throws Throwable
+     */
+    public function minus(string|int|float|Money $amount): Money
+    {
+        return new self($this->getAmount() - $this->ensureAmount($amount));
+    }
+
+    /**
+     * Check if this money amount is lower than input amount
+     *
+     * @throws Throwable
+     */
+    public function isLowerThan(string|int|float|Money $amount): bool
+    {
+        return $this->getAmount() < $this->ensureAmount($amount);
+    }
+
+    /**
+     * Check if this money amount is greater than input amount
+     *
+     * @throws Throwable
+     */
+    public function isGreaterThan(string|int|float|Money $amount): bool
+    {
+        return $this->getAmount() > $this->ensureAmount($amount);
+    }
+
+    /**
+     * Check if this money amount equals to input amount
+     *
+     * @throws Throwable
+     */
+    public function isEqualTo(string|int|float|Money $amount): bool
+    {
+        return $this->getAmount() === $this->ensureAmount($amount);
+    }
+}
